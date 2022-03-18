a9588465c33372fb5be61cce2aab8b63378408a8
Gabiden Abylay <32352131+Razbeg@users.noreply.github.com>
2022-03-18 10:58:39 +0600

    Update Task9LocatorsTest.cs

--- Task9/Task9/Task9LocatorsTest.cs
+++ Task9/Task9/Task9LocatorsTest.cs
@@ -11,11 +11,11 @@ namespace Task9
     {
         private IWebDriver _driver;
 
-        private readonly By _searchLocator = By.XPath("//a[@id='orbit-search-button']");
-        private readonly By _navigationLocator = By.XPath("//a[@class='sp-c-sport-navigation__link qa-primary-item sp-nav-click-stat']");
-        private readonly By _signIn = By.XPath("//div[@id='idcta-statusbar']");
-        private readonly By _socialLinkTwitter = By.XPath("//a[@class='sp-c-fuh-social sp-c-fuh-social--twitter']");
-        private readonly By _socialLinkYoutube = By.XPath("//a[@class='sp-c-fuh-social sp-c-fuh-social--youtube']");
+        private readonly By _searchLocator = By.Id("orbit-search-button");
+        private readonly By _homeTab = By.XPath("//div[@aria-label='Primary']//li[1]");
+        private readonly By _signIn = By.Id("idcta-statusbar");
+        private readonly By _socialLinkTwitter = By.XPath("//a[@href='https://twitter.com/BBCSport']");
+        private readonly By _socialLinkYoutube = By.XPath("//a[4]");
 
         [SetUp]
         public void StartBrowser()
@@ -26,6 +26,12 @@ namespace Task9
             _driver.Navigate().GoToUrl("https://www.bbc.com/sport");
         }
 
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Quit();
+        }
+
         [Test]
         public void SearchTest()
         {
@@ -33,11 +39,9 @@ namespace Task9
         }
 
         [Test]
-        public void NavigationTabTest()
+        public void HomeTabTest()
         {
-            var elements = _driver.FindElements(_navigationLocator);
-
-            elements[new Random().Next(0, elements.Count)].Click();
+            _driver.FindElement(_homeTab).Click();
         }
 
         [Test]
@@ -56,12 +60,6 @@ namespace Task9
         public void SocialLinkYoutubeTest()
         {
             Assert.IsNotNull(_driver.FindElement(_socialLinkYoutube));
-        }
-
-        [TearDown]
-        public void CloseBrowser()
-        {
-            _driver.Quit();
         }
     }
 }
